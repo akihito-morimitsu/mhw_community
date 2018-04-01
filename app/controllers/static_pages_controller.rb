67f1f04a17f271455f6e795066c5c62ca94e6e01
EC2 Default User <ec2-user@ip-172-31-22-254.ap-southeast-1.compute.internal>
2018-04-01 14:11:14 +0000

    fix post_path

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -19,6 +19,9 @@ class StaticPagesController < ApplicationController
   def contact
   end
   
-  
+  def post
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+  end
 end
 
